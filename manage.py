a9de9bdd9f8d67c5397f1b74156e3d7d7f0e7c85
Nickghost inc <you@example.com>
2018-09-15 21:07:36 +0300

    manage.py updated

---
+++ manage.py
@@ -0,0 +1,28 @@
+import unittest
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Role, Photo, Blog, Comment
+from flask_migrate import Migrate, MigrateCommand
+
+# create app instance 
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+@manager.command
+def test():
+    '''
+    run unittests
+    '''
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity = 2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Role = Role, Photo = Photo, Blog = Blog, Comment = Comment)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
