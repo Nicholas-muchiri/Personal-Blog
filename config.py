97282bef4fa03b7f81a564aff74eb046c612f6b0
Nickghost inc <you@example.com>
2018-09-18 10:14:22 +0300

    config and manage.py updated

--- config.py
+++ config.py
@@ -2,45 +2,59 @@ import os
 
 class Config:
     '''
-    parent configuration class
+    General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # simple mde configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
-    # email configurations
-    MAIL_SERVER = 'smtp.gmail.com'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
-    MAIL_USER_TLS = True
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'the C blog'
+    SENDER_EMAIL = 'wanjohi.carol75@gmail.com'
 
 class ProdConfig(Config):
     '''
-    child configuration class for production
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    Pruduction configuration child class
 
-class DevConfig(Config):
+    Args:
+        Config: The parent configuration class with General configuration settings
     '''
-    child configuration class for development
-    '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nicholas:golfgti10@localhost/blog'
-
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestConfig(Config):
     '''
-    child configuration class for tests
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
     '''
-    pass
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:carol1@localhost/personal_bog_test'
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:carol1@localhost/personal_bog'
+
+    DEBUG = True
+
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,28 +1,39 @@
-import unittest
-from app import create_app, db
-from flask_script import Manager, Server
-from app.models import User, Role, Photo, Blog, Comment
-from flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from flask_script import Manager,Server
+# Connect to models
+from app.models import User,Role
+# Set up migrations
+from flask_migrate import Migrate,MigrateCommand
 
-# create app instance 
+# Creating app instance
+# app = create_app('test')
 app = create_app('development')
+# app = create_app('production')
 
+
+# Create manager instance 
 manager = Manager(app)
-manager.add_command('server', Server)
+
+# Create migrate instance
+migrate = Migrate(app,db)
+
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
 @manager.command
 def test():
     '''
-    run unittests
+    Run the unit tests
     '''
+    import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity = 2).run(tests)
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Role = Role, Photo = Photo, Blog = Blog, Comment = Comment)
+    return dict( app=app, db=db, User=User, Role=Role)
 
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
